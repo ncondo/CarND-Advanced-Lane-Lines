3058ac41912101c3d46c33989d531b770c76f9a5
Nick <nicholas.condo@gmail.com>
2017-02-11 15:38:39 -0500

    Calibrate camera and save results in calibration_pickle.p

---
+++ camera_cal.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+import glob
+import pickle
+
+
+# Prepare object points, like (0,0,0), (1,0,0), ..., (8,5,0)
+objp = np.zeros((9*6, 3), np.float32)
+objp[:,:2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+
+# Arrays to store object opints and image points from all the images
+objpoints = [] # 3D points in real world space
+imgpoints = [] # 2D points in image plane
+
+# Make a list of calibration images
+images = glob.glob('./camera_cal/calibration*.jpg')
+
+# Step through the list and search for chessboard corners
+for idx, fname in enumerate(images):
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Find the chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, (9,6), None)
+
+    # If found, add object points and image points
+    if ret == True:
+        print('working on ',fname)
+        objpoints.append(objp)
+        imgpoints.append(corners)
+
+        # Draw and display the corners
+        cv2.drawChessboardCorners(img, (9,6), corners, ret)
+        write_name = 'camera_cal/corners_found'+str(idx)+'.jpg'
+        cv2.imwrite(write_name, img)
+
+# Load image for reference
+img = cv2.imread('./camera_cal/calibration1.jpg')
+img_size = (img.shape[1], img.shape[0])
+
+# Perform camera clibration given object points and image points
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
+
+# Save the camera calibration result for later use
+dist_pickle = {}
+dist_pickle['mtx'] = mtx
+dist_pickle['dist'] = dist
+pickle.dump(dist_pickle, open('./camera_cal/calibration_pickle.p', 'wb'))
