4bf42833df0c0797b0f58d552f28523de1a6739b
Nick <nicholas.condo@gmail.com>
2017-02-13 00:42:03 -0500

    Perform perspective transform on test images

--- image_generator.py
+++ image_generator.py
@@ -61,7 +61,7 @@ def mag_thresh(img, sobel_kernel=3, thresh=(0, 255)):
 
 def dir_thresh(img, sobel_kernel=3, thresh=(0, np.pi/2)):
     """
-    Returns an array of the same size as the input image of ones where gradients
+    Returns a binary image of the same size as the input image of ones where gradient directions
     were in the threshold range, and zeros everywhere else.
     :param img: input image in rgb format.
     :param sobel_kernel: size of the sobel kernel to apply (must be odd number >= 3).
@@ -74,7 +74,7 @@ def dir_thresh(img, sobel_kernel=3, thresh=(0, np.pi/2)):
     sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
     # Use np.arctan2(abs_sobely, abs_sobelx) to calculate direction of gradient
     absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
-    # Create a mask of 1's where the scaled gradient magnitude is within the given thresholds
+    # Create a mask of 1's where the gradient direction is within the given thresholds
     dir_binary = np.zeros_like(absgraddir)
     dir_binary[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
     # Return binary output image
@@ -83,7 +83,7 @@ def dir_thresh(img, sobel_kernel=3, thresh=(0, np.pi/2)):
 
 def color_thresh(img, s_thresh=(0, 255)):
     """
-    Returns binary image of the same size as the input image of ones where gradients
+    Returns a binary image of the same size as the input image of ones where pixel values
     were in the threshold range, and zeros everywhere else.
     :param img: input image in rgb format.
     :param s_thresh: threshold (0 to 255) for determining which pixels from s_channel to include when creating binary output.
@@ -93,10 +93,51 @@ def color_thresh(img, s_thresh=(0, 255)):
     # Apply a threshold to the S channel
     s_channel = hls[:,:,2]
     s_binary = np.zeros_like(s_channel)
+    # Create a mask of 1's where pixel value is within the given thresholds
     s_binary[(s_channel > s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
     # Return binary output image
     return s_binary
 
 
+def window_mask(width, height, img_ref, center, level):
+    output = np.zeros_like(img_ref)
+    output[int(img_ref.shape[0]-(level+1)*height):int(img_ref.shape[0]-level*height), max(0, int(center-width))]
+
+
+# Make a list of test images
+images = glob.glob('./test_images/test*.jpg')
+
+for idx, fname in enumerate(images):
+    img = cv2.imread(fname)
+    # Undistort the image
+    img = cv2.undistort(img, mtx, dist, None, mtx)
+    # Process image and generate binary pixels of interest
+    preprocess_image = np.zeros_like(img[:,:,0])
+    gradx = abs_sobel_thresh(img, orient='x', thresh=(12,255))
+    grady = abs_sobel_thresh(img, orient='y', thresh=(25,255))
+    color_binary = color_thresh(img, s_thresh=(100,255))
+    preprocess_image[((gradx == 1) & (grady ==1) | (color_binary == 1))] = 255
+
+    # Define perspective transform area
+    img_size = (img.shape[1], img.shape[0])
+    bot_width = .76 # percent of bottom trapizoid width
+    mid_width = .08 # percent of middle trapizoid width
+    height_pct = .62 # percent of trapizoid height
+    bottom_trim = .935 # percent from top to bottom to exclude hood of car
+    src = np.float32([[img.shape[1]*(.5-mid_width/2), img.shape[0]*height_pct], [img.shape[1]*(.5+mid_width/2), img.shape[0]*height_pct], [img.shape[1]*(.5+bot_width/2), img.shape[0]*bottom_trim], [img.shape[1]*(.5-bot_width/2), img.shape[0]*bottom_trim]])
+    offset = img_size[0] * .25
+    dst = np.float32([[offset, 0], [img_size[0]-offset, 0], [img_size[0]-offset, img_size[1]], [offset, img_size[1]]])
+
+    # Perform the transform
+    M = cv2.getPerspectiveTransform(src, dst)
+    Minv = cv2.getPerspectiveTransform(dst, src)
+    warped = cv2.warpPerspective(preprocess_image, M, img_size, flags=cv2.INTER_LINEAR)
+
+    
+    result = warped
+
+    # Save undistorted files to disk
+    write_name = './test_images/tracked'+str(idx+1)+'.jpg'
+    cv2.imwrite(write_name, result)
 
 
